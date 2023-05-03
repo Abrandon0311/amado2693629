c80fc875e7ede60dbd7167124ed60b0833efc411
brandon <ewduaramadov@gmail.com>
2023-05-03 11:38:55 -0500

    ACTIVIDADES LISTA 02 MAYO 2023

--- TAREAS/TAREAS/Listas/Lista2.py
+++ TAREAS/TAREAS/Listas/Lista2.py
@@ -6,8 +6,9 @@ print(tamaño)
 suma=0
 num_mayor=0
 num_menor=999
+mediana=0
 for i in range(tamaño):
-    num=int(random.randrange(50))
+    num=(random.round()*100)
     lista.append(num)
     suma += num
     promedio= suma / tamaño
@@ -15,11 +16,13 @@ for i in range(tamaño):
         num_mayor= num
     if num < num_menor:
         num_menor= num    
+    if num >0.0:
+        mediana=(num_mayor + num_menor) / 2  
+
     
 print(lista)
 print(f"la suma de los numeros en la lista es {suma}")
 print(f"el pormedio de la suma de los numeros es {promedio}")
 print(f"el numero mayor generado en la lista es {num_mayor}")
 print(f"el numero menor generado en la lista es {num_menor}")
-
-
+print(f"la mediana de los numeros es {mediana}")
